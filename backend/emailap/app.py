5a69c000b8df4718a1703bffc0ea7aae3f9211c7
bakyt123 <bunckerjeik@gmail.com>
2022-12-25 21:50:00 +0300

    kuba25.12.2022

--- backend/emailap/app.py
+++ backend/emailap/app.py
@@ -3,9 +3,9 @@ from email.mime.text import MIMEText
 
 
 def send_email(message):
-    sender = 'bunckerjeik@gmail.com'
-    password = 'ydtmtblgdtkssokv'
-    sender2 = input('komu: ')
+    sender = 'martingasparan115@gmail.com'
+    password = 'zxkdkebuhqhfqijd'
+    sender2 = 'bumeshov@yandex.ru'
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
@@ -13,7 +13,7 @@ def send_email(message):
     try:
         server.login(sender, password)
         msg = MIMEText(message)
-        msg['Subject'] = 'HI BAKYT!'
+        msg['Subject'] = 'SABRINA...'
         server.sendmail(sender, sender2, msg.as_string())
 
         return 'the message was sent succesfullt'
@@ -21,11 +21,13 @@ def send_email(message):
         return f'{_ex}\nCheck your login or password!'
 
 
-def main():
-    message = input('Type your message: ')
+
+x = 0
+while x < 50:
+    x += 1
+    message = 'DO YOU REMEMBER WHAT YOU DID LAST SUMMER???? I AM))) '
     print(send_email(message=message))
 
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__call__':
+    call()
